ebfe6701eda51cb259cbf74adf722ab4bc07f2a7
Md Shobuj Mia <md.shobuj.developer@gmail.com>
2024-02-01 18:12:10 +0600

    update image location

--- src/script.js
+++ src/script.js
@@ -1,13 +1,7 @@
 const navLinks = document.querySelector('.nav-links');
 let navLinkItems = document.querySelectorAll('.nav-items');
 function onToggleMenu(e){
-    // if (e.src = '/assets/images/toggle icons/burger-bar.png') {
-    //     e.src = '/assets/images/toggle icons/close.png';
-    //   } else {
-    //     e.src = '/assets/images/toggle icons/burger-bar.png';
-    //   }
     
-    //   e.src = '/assets/images/toggle icons/burger-bar.png'? e.src = '/assets/images/toggle icons/close.png':e.src = '/assets/images/toggle icons/burger-bar.png';
 
     navLinks.classList.toggle('top-[50px]');
     
